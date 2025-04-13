b285d1aa9b7f9b1739afe7b81d9125907ab96d9c
Shraban-7 <shakuatshraban@gmail.com>
2025-04-13 17:15:01 +0600

    Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Seller;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $data = [];
+        $data['total_products'] = Product::count();
+        $data['total_orders'] = Order::count();
+        $data['pending_orders'] = Order::pending()->count();
+        $data['shipped_orders'] = Order::shipped()->count();
+        $data['cancelled_orders'] = Order::cancelled()->count();
+        $data['delivered_orders'] = Order::delivered()->count();
+        $data['total_sales'] = Order::delivered()->sum('payable');
+        $data['total_sellers'] = Seller::count();
+        $data['total_customers'] = Order::distinct('user_id')->count('user_id');
+
+        return view('admin.dashboard', $data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,137 @@
 @section('title', 'Dashboard')
 @section('content')
 
-<h4>Welcome Admin!!</h4>
+<div class="row">
+    <div class="col-12 mb-5">
+        <div class="row row-cols-lg-3 row-cols-2 g-lg-5 g-2">
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Pending Orders</span>
+                            <i data-feather="clock" class="text-warning"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $pending_orders }}</h3>
+                        </div>
+                        <a href="{{ route('seller.orders.cancelled') }}"><small>View Orders</small> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Delivered Orders</span>
+                            <i data-feather="check-circle" class="text-success"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $delivered_orders }}</h3>
+                        </div>
+                        <a href="{{ route('seller.orders.delivered') }}"><small>View Orders</small> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Shipped Orders</span>
+                            <i data-feather="truck" class="text-primary"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $shipped_orders }}</h3>
+                        </div>
+                        <a href="{{ route('seller.orders.shipped') }}"><small>View Orders</small> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Cancelled Orders</span>
+                            <i data-feather="x-circle" class="text-danger"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $cancelled_orders }}</h3>
+                        </div>
+                        <a href="{{ route('seller.orders.cancelled') }}"><small>View Orders</small> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Total Products</span>
+                            <i data-feather="box" class="text-info"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $total_products }}</h3>
+                        </div>
+                        <a href="{{ route('seller.products.index') }}"><small>Total products listed</small></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Total Orders</span>
+                            <i data-feather="shopping-cart" class="text-success"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $total_orders }}</h3>
+                        </div>
+                        <small>Orders received</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Total Sales</span>
+                            <i data-feather="dollar-sign" class="text-success"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ currency($total_sales) }}</h3>
+                        </div>
+                        <small>Total earnings from sales</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Total Sellers</span>
+                            <i data-feather="dollar-sign" class="text-success"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $total_sellers }}</h3>
+                        </div>
+                        <small>Total earnings from sales</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 card-lift">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-semi-bold">Total Customers</span>
+                            <i data-feather="users" class="text-primary"></i>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <h3 class="fw-bold mb-0">{{ $total_customers }}</h3>
+                        </div>
+                        <small>Registered customers</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
