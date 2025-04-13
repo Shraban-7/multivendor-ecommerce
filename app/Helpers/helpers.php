1d44a07ea84ea3cf2c95da1b575a96508065f0a9
Shraban-7 <shakuatshraban@gmail.com>
2025-04-13 04:17:26 +0000

    commit from live

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,200 @@
+<?php
+
+use App\Models\Category;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Number;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+define('CURRENCY_SYMBOL', '৳');
+
+if (!function_exists('redirect_intended')) {
+    function redirect_intended($default = '/')
+    {
+        return redirect()->intended($default);
+    }
+}
+
+if (!function_exists('str_slug')) {
+    function str_slug($table, $column, $title, $separator = '-')
+    {
+        $slug = Str::slug($title, $separator);
+        $originalSlug = $slug;
+        $count = 1;
+
+        while (DB::table($table)->where($column, $slug)->exists()) {
+            $slug = "{$originalSlug}{$separator}{$count}";
+            $count++;
+        }
+
+        return $slug;
+    }
+}
+
+
+if (!function_exists('generateFileName')) {
+    function generateFileName($file)
+    {
+        return time() . rand(1, 9999) . '.' . $file->extension();
+    }
+}
+
+if (!function_exists('upload_file')) {
+    function upload_file($file, $directory, $disk = 'public')
+    {
+        if (!Storage::disk($disk)->exists($directory)) {
+            Storage::disk($disk)->makeDirectory($directory);
+        }
+
+        $fileName =  time() . rand(1, 9999) . '.' . $file->getClientOriginalExtension();
+        $path = $directory . '/' . $fileName;
+        Storage::disk($disk)->put($path, File::get($file));
+
+        return $path;
+    }
+}
+
+if (!function_exists('storage_url')) {
+    function storage_url($file, $disk = 'public')
+    {
+        return Storage::disk($disk)->url($file);
+    }
+}
+
+if (!function_exists('delete_file')) {
+    function delete_file($file)
+    {
+        Storage::disk('public')->delete($file);
+    }
+}
+
+if (!function_exists('nav_categories')) {
+    function nav_categories()
+    {
+        return Category::nav()->orderBy('id', 'DESC')->get();
+    }
+}
+
+if (!function_exists('all_department_categories')) {
+    function all_department_categories()
+    {
+        $categories = Category::allDepartment()
+            ->category()
+            ->orderBy('name', 'ASC')
+            ->with('subcategories')
+            ->get();
+
+        return $categories;
+    }
+}
+
+if (!function_exists('currency')) {
+    function currency($value)
+    {
+        return CURRENCY_SYMBOL.' '.number_format($value,2);
+    }
+}
+
+if (!function_exists('number_shorten_format')) {
+    function number_shorten_format($number, $precision = 1, $divisors = null)
+    {
+        if (!isset($divisors)) {
+            $divisors = array(
+                pow(1000, 0) => '',
+                pow(1000, 1) => 'K',
+                pow(1000, 2) => 'M',
+                pow(1000, 3) => 'B',
+            );
+        }
+        foreach ($divisors as $divisor => $shorthand) {
+            if (abs($number) < ($divisor * 1000)) {
+                break;
+            }
+        }
+        if($number<= 1000)
+        {
+            $precision = 0;
+        }
+        return number_format($number / $divisor, $precision) . $shorthand;
+    }
+}
+
+if (!function_exists('datetime_format')) {
+    function datetime_format($time)
+    {
+        $carbonTime = Carbon::parse($time);
+        $days = $carbonTime->diffInDays(Carbon::now()) > 0 ? $carbonTime->diffInDays(Carbon::now()) . ':' : '';
+        $formattedTime = substr($carbonTime->format('H:i:s.u'), 0, -3);
+        return $days . $formattedTime;
+    }
+}
+
+if (!function_exists('percentage')) {
+    function percentage($number)
+    {
+        return Number::percentage($number);
+    }
+}
+
+if (!function_exists('seller')) {
+    function seller() {
+        return Auth::guard('seller')->user();
+    }
+}
+
+if (!function_exists('apiResponse')) {
+    function apiResponse(object|array $data, string|null $message = null, int $statusCode = 200)
+    {
+        $response['status'] = true;
+
+        if (isset($message)) $response['message'] = $message;
+        if (!empty($data)) $response['data'] = $data;
+
+        return response()->json($response, $statusCode);
+    }
+}
+
+if (!function_exists('successResponse')) {
+    function successResponse(string $message, int $statusCode = 200)
+    {
+        $response['status'] = true;
+
+        if (isset($message)) $response['message'] = $message;
+
+        return response()->json($response, $statusCode);
+    }
+}
+
+if (!function_exists('errorResponse')) {
+    function errorResponse(string $message, int $statusCode = 400)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message ?? 'Something went wrong!',
+        ], $statusCode);
+    }
+}
+
+if (!function_exists('apiResourceResponse')) {
+    function apiResourceResponse(object $collection, string|null $message = null, array $extraData = [], int $statusCode = 200)
+    {
+        $response['status'] = true;
+        if (isset($message)) $response['message'] = $message;
+        if (!empty($extraData)) $response['extraData'] = $extraData;
+        if (!empty($collection)) {
+            $collection = $collection->additional($response)->response()->getData();
+        }
+
+        return response()->json($collection, $statusCode);
+    }
+}
+
+if (!function_exists('money')) {
+    function money($amount)
+    {
+        return number_format($amount, 2) .' ' .  CURRENCY_SYMBOL;
+    }
+}
---
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductAttribute;
+
+class CategoryController extends Controller
+{
+    public function category_details($slug, Request $request)
+    {
+        $category = Category::where('slug', $slug)
+            ->with(['products.productAttributes.options', 'subcategories'])
+            ->first();
+
+        $query = $category->products();
+
+        if ($request->has('subcategory') && $request->subcategory != 'all') {
+            $query->whereHas('subcategory', function ($q) use ($request) {
+                $q->where('slug', $request->subcategory);
+            });
+        }
+
+        $productAttributes = ProductAttribute::with('options')->get()->unique('name');
+
+        // return $product_attributes;
+
+        foreach ($request->all() as $key => $value) {
+            if (in_array($key, $productAttributes->pluck('name')->map('strtolower')->toArray()) && $value != 'all') {
+                $query->whereHas('productAttributes.options', function ($q) use ($key, $value) {
+                    $q->where('name', ucfirst($key))->where('value', $value);
+                });
+            }
+        }
+
+        $products = $query->with('productAttributes.options')->get();
+
+        return view('frontend.pages.category_detail', compact('category', 'productAttributes', 'products'));
+    }
+}
