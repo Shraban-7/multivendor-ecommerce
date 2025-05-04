e2d4b328a705d5e277a2e0cf281b1e845ba50879
Shraban-7 <shakuatshraban@gmail.com>
2025-05-04 15:18:27 +0600

    cart details quick view modal add

--- resources/views/frontend/cart/details.blade.php
+++ resources/views/frontend/cart/details.blade.php
@@ -234,66 +234,7 @@
 
                         <div class="grid grid-cols-1 gap-3 xsm:grid-cols-2 md:grid-cols-3 sm:gap-6">
                             <!-- Product Card 1 -->
-                            @foreach ($products as $product)
-                                <div
-                                    class="relative text-base xsm:text-sm sm:text-base md:text-sm lg:text-sm xl:text-base rounded-xl hover:shadow-lg eq">
-                                    <div
-                                        class="relative overflow-hidden rounded-lg h-60 xsm:h-48 sm:h-56 lg:h-56 xl:h-64 2xl:h-60">
-                                        <a href="{{ route('products.details', $product->slug) }}"
-                                            class="block w-full h-full">
-                                            <img src="{{ storage_url($product->thumbnail) }}"
-                                                alt="ASUS Vivo15 OLED K513 Core-i5 11th Gen 15.6″ FHD Laptop"
-                                                class="object-cover w-full h-full" />
-                                        </a>
-                                        <button
-                                            class="absolute flex items-center gap-2 px-4 py-2 bg-white rounded-full shadow-lg bottom-10 xsm:bottom-3 lg:bottom-8 xsm:left-3 lg:left-5 left-5 hover:bg-primary hover:text-white eq">
-                                            <i class="fa-regular fa-eye"></i>
-                                            Quick View
-                                        </button>
-                                    </div>
-
-                                    <div class="p-4 xsm:p-2 lg:p-5">
-                                        <h3 class="font-medium lg:mb-2 xl:mb-0 xsm:h-10 sm:h-12 md:h-10 lg:h-14 xl:h-12">
-                                            <a href="{{ route('products.details', $product->slug) }}"
-                                                class="line-clamp-2 lg:line-clamp-3 xl:line-clamp-2 hover:text-primary eq">{{ $product->name }}</a>
-                                        </h3>
-                                        <p class="text-leaf-green">Almost sold Out</p>
-
-                                        <div class="flex flex-wrap items-center gap-x-1">
-                                            <div class="flex flex-no-wrap items-center gap-x-1 text-light-yellow">
-                                                <i class="fa-solid fa-star"></i>
-                                                <i class="fa-solid fa-star"></i>
-                                                <i class="fa-solid fa-star"></i>
-                                                <i class="fa-solid fa-star"></i>
-                                                <i class="fa-solid fa-star"></i>
-                                                <img src="{{ asset('assets/frontend/images/fire-icon.png') }}"
-                                                    class="w-8 h-auto" alt="Fire Icon" />
-                                            </div>
-                                            <span class="text-jet-gray">{{ $product->stock_out }}+ Sold</span>
-                                        </div>
-
-                                        <div class="flex flex-wrap items-center gap-x-5 xsm:gap-x-1 sm:gap-x-2 xl:mt-2">
-                                            <span class="text-primary/80">Final Hours</span>
-                                            <div class="flex items-center gap-x-5 xsm:gap-x-2 sm:gap-x-5 xl:gap-x-8">
-                                                <div class="flex flex-no-wrap items-center gap-1 price">
-                                                    <i class="fa-solid fa-bolt text-[#ffa755]"></i>
-
-                                                    <h3 class="font-bold text-primary">
-                                                        {{ money($product->selling_price) }}</h3>
-                                                </div>
-                                                <div>
-                                                    <input type="hidden" name="quantity" value="1"
-                                                        id="qtyInput{{ $product->id }}">
-                                                    <button data-id="{{ $product->id }}" type="button"
-                                                        class="cartBtn text-xs xsm:text-[10px] sm:text-base md:text-xs xl:text-base w-7 h-7 xsm:w-6 xsm:h-6 md:w-8 md:h-8 sm:w-10 sm:h-10 xl:w-10 xl:h-10 flex items-center justify-center bg-primary rounded-full text-white hover:bg-theme-dark eq">
-                                                        <i class="fa-solid fa-cart-plus"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
+                            @include('frontend.partials.product-card-load', ['products' => $products])
                         </div>
                     </div>
                 </div>
