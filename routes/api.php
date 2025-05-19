48cb345e736a753eb26276f25ca752b66fdb9034
Shraban-7 <shakuatshraban@gmail.com>
2025-05-19 15:56:16 +0600

    category api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\SettingController;
 
@@ -17,6 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('logout', [AuthController::class, 'logout']);
 
+    Route::get('categories', [CategoryController::class, 'index']);
     Route::get('dashboard', [DashboardController::class, 'index']);
 
     // Route::get('categories', [ContentCategoryController::class, 'index']);
