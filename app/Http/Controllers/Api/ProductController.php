4e7683b032877eded3885a9965e8265e00f17cd6
Shraban-7 <shakuatshraban@gmail.com>
2025-05-20 10:56:28 +0600

    product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -9,9 +9,26 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::paginate(15);
+        $cateory_id = $request->category_id ?? '';
+        $subcateory_id = $request->subcateory_id ?? '';
+        $name = $request->name ?? '';
+
+        $products = Product::query();
+
+        if ($cateory_id != '') $products->where('cateory_id', $cateory_id);
+        if ($subcateory_id != '') $products->where('subcateory_id', $subcateory_id);
+
+        if ($name != '') {
+            $products->where(function ($query) use ($name) {
+                $query->where('name', $name)
+                    ->orWhere('name', 'LIKE', "{$name}%")
+                    ->orWhere('name', 'LIKE', "%{$name}%");
+            });
+        }
+
+        $products = $products->paginate(15)->appends($request->query());
 
         return apiResourceResponse(ProductListResource::collection($products));
     }
