7a613bb0eea44b60c47c74d07fa402e2e789db11
Shraban-7 <shakuatshraban@gmail.com>
2025-05-25 14:51:40 +0600

    Brands

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BrandResource;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductListResource;
 use App\Http\Resources\SellerResource;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Seller;
@@ -15,6 +17,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $data['brands'] = BrandResource::collection(Brand::get());
         $data['categories'] = CategoryResource::collection(Category::category()->get());
 
         $newProducts = Product::latest('id')->take(16)->get();
