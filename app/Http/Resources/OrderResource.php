1269bd2e9bcf322ad5c468dd50e8d7aefc979aad
Shraban-7 <shakuatshraban@gmail.com>
2025-05-26 15:39:23 +0600

    order Api

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'               => $this->id,
+            'invoice_id'       => $this->invoice_id,
+            'user_id'          => $this->user_id,
+            'seller_id'        => $this->seller_id,
+            'customer_name'    => $this->customer_name,
+            'customer_email'   => $this->customer_email,
+            'customer_phone'   => $this->customer_phone,
+            'customer_address' => $this->customer_address,
+            'sub_total'        => $this->sub_total,
+            'discount'         => $this->discount,
+            'tax'              => $this->tax,
+            'shipping_fee'     => $this->shipping_fee,
+            'total'            => $this->total,
+            'payable'          => $this->payable,
+            'due'              => $this->due,
+            'status'           => $this->status,
+            'delivery_status'  => $this->delivery_status,
+            'items'            => OrderItemResource::collection($this->whenLoaded('items')),
+        ];
+    }
+}
