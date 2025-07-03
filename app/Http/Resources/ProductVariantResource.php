062f8313b24ae5c13c7ab32b22353567201b1348
Shraban-7 <shakuatshraban@gmail.com>
2025-07-03 11:16:06 +0600

    price casting

--- app/Http/Resources/ProductVariantResource.php
+++ app/Http/Resources/ProductVariantResource.php
@@ -12,20 +12,21 @@ class ProductVariantResource extends JsonResource
     public function __construct($resource, $defaultVariantId = null)
     {
         parent::__construct($resource);
+        
         $this->defaultVariantId = $defaultVariantId;
     }
 
     public function toArray(Request $request): array
     {
         return [
-            'id'               => $this->id,
-            'sku'              => $this->sku,
-            'stock'            => $this->stock_in - $this->stock_out,
-            'price'            => (string) $this->selling_price,
-            'discounted_price' => (string) $this->discounted_price,
-            'image'            => $this->image,
-            'value_ids'        => $this->optionValues->pluck('id')->sort()->values()->toArray(),
-            'default'          => $this->id === $this->defaultVariantId,
+            'id' => $this->id,
+            'sku' => $this->sku,
+            'stock' => $this->stock_in - $this->stock_out,
+            'price' => removeZeroFromDecimal($this->selling_price),
+            'discounted_price' => removeZeroFromDecimal($this->discounted_price),
+            'image' => $this->image,
+            'value_ids' => $this->optionValues->pluck('id')->sort()->values()->toArray(),
+            'default' => $this->id === $this->defaultVariantId,
         ];
     }
 }
