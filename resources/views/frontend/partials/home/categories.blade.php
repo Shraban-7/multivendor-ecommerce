032c6ab023a1a70db71a98fba3ba0bfaf48ec77d
Shraban-7 <shakuatshraban@gmail.com>
2025-08-03 16:49:30 +0600

    categories

--- resources/views/frontend/partials/home/categories.blade.php
+++ resources/views/frontend/partials/home/categories.blade.php
@@ -5,14 +5,16 @@
 
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6">
         @foreach ($categories as $category)
-        <a href="{{ route('category.details',$category->slug) }}">
-            <div class="flex flex-col items-center p-4 bg-white rounded-lg shadow hover:shadow-lg cursor-pointer transition">
+
+        <div class="flex flex-col items-center p-4 bg-white rounded-lg shadow hover:shadow-lg cursor-pointer transition">
+            <a href="{{ route('category.details',$category->slug) }}">
                 <img src="{{ storage_url($category->image) }}" alt="{{ $category->name }}" class="w-16 h-16 mb-3 object-contain" />
-                <span class="text-center font-medium text-gray-700 text-sm leading-tight break-words line-clamp-2">
+                <span class="text-center text-gray-700 text-sm leading-tight break-words line-clamp-2">
                     {{ $category->name }}
                 </span>
-            </div>
-        </a>
+            </a>
+        </div>
+
         @endforeach
     </div>
 </section>
