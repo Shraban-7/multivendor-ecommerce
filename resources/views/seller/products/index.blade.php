b27313ff76e6a0d88af19305cffd0ee661f85719
Shraban-7 <shakuatshraban@gmail.com>
2025-08-04 15:02:23 +0600

    Product list

--- resources/views/seller/products/index.blade.php
+++ resources/views/seller/products/index.blade.php
@@ -4,6 +4,9 @@
 
 <div class="mb-3 d-flex justify-content-between align-items-end">
     <h4 class="mb-0">Products</h4>
+    <a href="{{ route('seller.products.create') }}" class="btn btn-primary btn-sm">
+        <i data-feather="plus" class="icon-xs me-1"></i> Add Product
+    </a>
 </div>
 
 <div class="table-responsive">
@@ -66,11 +69,16 @@
 
                 </td>
                 <td>{{ $product->created_at->format('d-m-y h:i A') }} </td>
-                <td class="d-flex">
-                    <a href="{{ route('seller.products.show', $product->slug) }}"
-                        class="border btn btn-light btn-sm me-1" title="Details">
-                        <i data-feather="eye" class="icon-xs"></i> Details
-                    </a>
+                <td>
+                    <div class="d-flex">
+                        <a href="{{ route('seller.products.show', $product->slug) }}"
+                            class="border btn btn-light btn-sm me-1" title="Details">
+                            <i data-feather="eye" class="icon-xs"></i> Details
+                        </a>
+                        <a href="{{ route('seller.products.edit', $product->slug) }}" class="btn btn-primary btn-sm w-lg-auto">
+                            <i data-feather="edit" class="icon-xs me-1"></i> Edit
+                        </a>
+                    </div>
                 </td>
             </tr>
             @endforeach
@@ -151,6 +159,7 @@
         </div>
     </div>
 </div>
+
 @push('scripts')
 <script>
     new DataTable('#product-table', {
