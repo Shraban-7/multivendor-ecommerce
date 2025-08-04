3225ee9d6bde6d4b0d98da87e4fcdf384c8fafb1
Shraban-7 <shakuatshraban@gmail.com>
2025-08-04 17:00:27 +0600

    favicon

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -9,7 +9,7 @@ $settings = settings();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset($settings->favicon) }}">
+    <x-favicons/>
     <script src="{{ asset('assets/libs/jquery/jquery-3.7.1.min.js') }}"></script>
     @vite('resources/css/app.css')
 
