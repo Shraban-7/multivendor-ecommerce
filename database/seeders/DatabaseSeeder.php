4bf3403bef5bd4087d2ac04601e4f4d18d579262
Shraban-7 <shakuatshraban@gmail.com>
2025-08-07 15:11:56 +0600

    Geolocations added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -40,6 +39,11 @@ class DatabaseSeeder extends Seeder
             SystemSettingSeeder::class,
             PermissionSeeder::class,
             PaymentGatewaySeeder::class,
+
+            DivisionSeeder::class,
+            DistrictSeeder::class,
+            UpazilaSeeder::class,
+            UnionSeeder::class,
         ]);
     }
 }
