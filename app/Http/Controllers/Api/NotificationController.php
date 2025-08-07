72ae7444e4564fdceed58a9806167c9878956a81
Shraban-7 <shakuatshraban@gmail.com>
2025-08-07 11:16:57 +0600

    api notification

---
+++ app/Http/Controllers/Api/NotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationResource;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+        $notifications = Notification::where('user_id', Auth::id())->latest('id')->get();
+
+        return apiResourceResponse(NotificationResource::collection($notifications));
+    }
+}
