00a7639dfb3dddf10f02e7b610d0e94c3725e121
Shraban-7 <shakuatshraban@gmail.com>
2025-08-11 11:22:42 +0600

    billing address relation

--- app/Models/BillingAddress.php
+++ app/Models/BillingAddress.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BillingAddress extends Model
 {
@@ -11,5 +12,13 @@ class BillingAddress extends Model
 
     protected $guarded = ['id'];
 
-    
+    public function division(): BelongsTo
+    {
+        return $this->belongsTo(Division::class);
+    }
+
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(District::class);
+    }
 }
