a55df8370d6257e7fd75497f0d9b8bfe0df4420b
Shraban-7 <shakuatshraban@gmail.com>
2025-08-14 13:11:38 +0600

    payment gateway

--- app/Http/Resources/PaymentGatewayResource.php
+++ app/Http/Resources/PaymentGatewayResource.php
@@ -7,20 +7,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PaymentGatewayResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'image' => storage_url($this->image),
-            'payment_url' => $this->payment_url,
-            'credentials' => $this->credentials,
-            'is_enable' => $this->is_enabled,
+            'is_enabled' => $this->is_enabled,
             'is_default' => $this->is_default
         ];
     }
