15bd6fa3d8cccee7bff1daa98a01b3e8b75bd929
Shraban-7 <shakuatshraban@gmail.com>
2025-08-14 14:58:30 +0600

    billing address

--- app/Models/BillingAddress.php
+++ app/Models/BillingAddress.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\AddressType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,6 +12,10 @@ class BillingAddress extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'type' => AddressType::class,
+    ];
 
     public function division(): BelongsTo
     {
