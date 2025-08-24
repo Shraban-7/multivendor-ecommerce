8dbcb347d0769951baa3252eb56122964ca898c7
Shraban-7 <shakuatshraban@gmail.com>
2025-08-24 13:18:52 +0600

    seller sign up

---
+++ database/migrations/2025_08_24_114856_add_division_district_column_to_sellers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sellers', function (Blueprint $table) {
+            $table->unsignedBigInteger('division_id')->nullable()->after('password');
+            $table->unsignedBigInteger('district_id')->nullable()->after('division_id');
+        });
+
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('division_id')->nullable()->after('password');
+            $table->unsignedBigInteger('district_id')->nullable()->after('division_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sellers', function (Blueprint $table) {
+            //
+        });
+    }
+};
