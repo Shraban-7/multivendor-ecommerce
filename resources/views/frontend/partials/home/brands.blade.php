388c6fa27d0f828bcf8c8c645d7e137a78c3d911
Shraban-7 <shakuatshraban@gmail.com>
2025-09-03 13:00:37 +0600

    home

--- resources/views/frontend/partials/home/brands.blade.php
+++ resources/views/frontend/partials/home/brands.blade.php
@@ -10,7 +10,7 @@
         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
             @foreach ($brands as $brand)
                 <div class="border  border-gray-100 bg-white p-6 flex flex-col items-center shadow-sm transition-shadow hover:shadow-md ">
-                    <img src="{{ storage_url($brand->image) }}" alt="Ford"
+                    <img src="{{ storage_url($brand->image) }}" alt="{{ $brand->name }}"
                         class="h-12 mb-4">
                     <span class="text-gray-700 font-medium">{{ $brand->name }}</span>
                 </div>
