2442f1f23a3fa799add66bf18746140d341ea7fe
Shraban-7 <shakuatshraban@gmail.com>
2025-12-01 23:33:18 +0600

    homepage

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     {
         $data['categories'] = Category::category()->get();
 
-        $data['special_category'] = Category::special()->with(['banners', 'products'])->first();
+        //$data['special_category'] = Category::special()->with(['banners', 'products'])->first();
 
         $new_arrival_products = Product::withDefaultRelations()
             ->active()
@@ -27,47 +27,47 @@ class HomeController extends Controller
             ->limit(16)
             ->get();
 
-        $data['new_arrival_products'] = $new_arrival_products->map(fn($product) => $product->toDetailsArray());
+        //$data['new_arrival_products'] = $new_arrival_products->map(fn($product) => $product->toDetailsArray());
 
-        $trending_products = Product::withDefaultRelations()
-            ->active()
-            ->withAvg('reviews', 'rating')
-            ->where('is_trending', 1)
-            ->withCount('reviews')
-            ->orderByDesc('id')
-            ->limit(16)
-            ->get();
+        // $trending_products = Product::withDefaultRelations()
+        //     ->active()
+        //     ->withAvg('reviews', 'rating')
+        //     ->where('is_trending', 1)
+        //     ->withCount('reviews')
+        //     ->orderByDesc('id')
+        //     ->limit(16)
+        //     ->get();
 
-        $data['trending_products'] = $trending_products->map(fn($product) => $product->toDetailsArray());
+        // $data['trending_products'] = $trending_products->map(fn($product) => $product->toDetailsArray());
 
-        $bestselling_products = Product::withDefaultRelations()
-            ->active()
-            ->withAvg('reviews', 'rating')
-            ->where('best_selling', 1)
-            ->withCount('reviews')
-            ->orderByDesc('id')
-            ->limit(10)
-            ->get();
+        // $bestselling_products = Product::withDefaultRelations()
+        //     ->active()
+        //     ->withAvg('reviews', 'rating')
+        //     ->where('best_selling', 1)
+        //     ->withCount('reviews')
+        //     ->orderByDesc('id')
+        //     ->limit(10)
+        //     ->get();
 
-        $data['bestselling_products'] = $bestselling_products->map(fn($product) => $product->toDetailsArray());
+        // $data['bestselling_products'] = $bestselling_products->map(fn($product) => $product->toDetailsArray());
 
-        $featured_products = Product::withDefaultRelations()
-            ->active()
-            ->withAvg('reviews', 'rating')
-            ->where('is_featured', 1)
-            ->withCount('reviews')
-            ->orderByDesc('id')
-            ->limit(10)
-            ->get();
+        // $featured_products = Product::withDefaultRelations()
+        //     ->active()
+        //     ->withAvg('reviews', 'rating')
+        //     ->where('is_featured', 1)
+        //     ->withCount('reviews')
+        //     ->orderByDesc('id')
+        //     ->limit(10)
+        //     ->get();
 
-        $data['featured_products'] = $featured_products->map(fn($product) => $product->toDetailsArray());
+        // $data['featured_products'] = $featured_products->map(fn($product) => $product->toDetailsArray());
 
-        $data['hero_grid_one']   = HeroBanner::where('position', 1)->first();
-        $data['hero_grid_two']   = HeroBanner::where('position', 2)->first();
-        $data['hero_grid_three'] = HeroBanner::where('position', 3)->first();
-        $data['hero_grid_four']  = HeroBanner::where('position', 4)->first();
-        $data['hero_grid_five']  = HeroBanner::where('position', 5)->first();
-        
+        // $data['hero_grid_one']   = HeroBanner::where('position', 1)->first();
+        // $data['hero_grid_two']   = HeroBanner::where('position', 2)->first();
+        // $data['hero_grid_three'] = HeroBanner::where('position', 3)->first();
+        // $data['hero_grid_four']  = HeroBanner::where('position', 4)->first();
+        // $data['hero_grid_five']  = HeroBanner::where('position', 5)->first();
+
         $data['hero_banners'] = HeroBanner::active()->orderBy('position')->get();
         $data['sellers'] = Seller::active()->limit(8)->get();
         $data['brands'] = Brand::where('status', 1)->orderBy('name')->limit(12)->get();
