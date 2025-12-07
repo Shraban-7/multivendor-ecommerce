ad73372fe389e50865e8aba5acbbe8417b7aa5cd
Shraban-7 <shakuatshraban@gmail.com>
2025-12-07 14:34:57 +0600

    static page

--- resources/views/frontend/static_page.blade.php
+++ resources/views/frontend/static_page.blade.php
@@ -1,5 +1,21 @@
 @extends('frontend.layouts.app')
-@section('title', 'Page')
+@section('title', $page->title)
 
 @section('content')
+<div class="container py-5">
+    <div class="row">
+        <div class="col-lg-10 mx-auto">
+            <h1 class="display-4 mb-4 text-center">{{ $page->title }}</h1>
+            <hr class="mb-5">
+            <div class="card shadow-sm p-4 p-md-5">
+                <div class="card-body">
+                    <div class="static-page-content">
+                        {!! $page->content !!}
+                    </div>
+                </div>
+            </div>
+            <!-- <p class="text-muted text-end mt-3">Last updated: {{ $page->updated_at->format('M d, Y') }}</p> -->
+        </div>
+    </div>
+</div>
 @endsection
