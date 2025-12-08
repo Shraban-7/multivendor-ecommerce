b5e913f4da2675cc4b9399f79656264be98390cf
Shraban-7 <shakuatshraban@gmail.com>
2025-12-08 15:33:01 +0600

    mobile nav product active nav color

--- resources/views/frontend/layouts/mobile-nav.blade.php
+++ resources/views/frontend/layouts/mobile-nav.blade.php
@@ -1,14 +1,14 @@
 <div
     class="md:hidden fixed bottom-0 left-0 w-full bg-white shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] border-t border-gray-100 z-40 px-6 py-3 flex justify-between items-center text-gray-400">
-    <a href="{{ route('home') }}" class="flex flex-col items-center gap-1 text-primary-600">
+    <a href="{{ route('home') }}" class="flex flex-col items-center gap-1 {{ request()->routeIs('home') ? 'text-primary-600': '' }} hover:text-primary-600">
         <i class="fas fa-home text-lg"></i>
         <span class="text-[10px] font-medium">Home</span>
     </a>
-    <a href="{{ route('products.index') }}" class="flex flex-col items-center gap-1 hover:text-primary-600">
+    <a href="{{ route('products.index') }}" class="flex flex-col items-center gap-1 {{ request()->routeIs('products.index') ? 'text-primary-600': '' }} hover:text-primary-600">
         <i class="fas fa-th-large text-lg"></i>
         <span class="text-[10px] font-medium">Shop</span>
     </a>
-    <a href="#" class="flex flex-col items-center gap-1 hover:text-primary-600 relative">
+    <a href="#" class="flex flex-col items-center gap-1 {{ request()->routeIs('cart.details') ? 'text-primary-600': '' }} hover:text-primary-600 relative">
         <div class="relative">
             <i class="fas fa-shopping-cart text-lg"></i>
             <span id="cartCount"
