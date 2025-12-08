af67327a7bc5e6753719403b6dc5f217523d8bff
Shraban-7 <shakuatshraban@gmail.com>
2025-12-08 13:14:11 +0600

    banner added to api

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -3,23 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BannerResource;
 use App\Http\Resources\BrandResource;
 use App\Http\Resources\CategoryResource;
 
 use App\Http\Resources\ProductListResource;
 use App\Http\Resources\SellerResource;
+use App\Models\Banner;
 use App\Models\Brand;
-use App\Models\Cart;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Seller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $banners = Banner::active()->where('section', Banner::SECTION_HERO)->get();
+        $data['banners'] = BannerResource::collection($banners);
         $data['brands'] = BrandResource::collection(Brand::get());
         $data['categories'] = CategoryResource::collection(Category::category()->limit(8)->get());
 
