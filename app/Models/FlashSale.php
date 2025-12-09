6f0fd2c2d22e5450d70739ded5a99567baabd37c
Shraban-7 <shakuatshraban@gmail.com>
2025-12-09 13:35:27 +0600

    flash sale

--- app/Models/FlashSale.php
+++ app/Models/FlashSale.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class FlashSale extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', 1);
+    }
 }
