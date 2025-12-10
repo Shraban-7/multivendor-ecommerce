892aa5e4e3110e837f9c6e71c8f66f66e0d19423
Shraban-7 <shakuatshraban@gmail.com>
2025-12-10 17:19:30 +0600

    admin middlewire redirection

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -17,7 +17,7 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (!Auth::guard('admin')->check()) {
-            return redirect('/login');
+            return redirect()->route('home');
         }
         return $next($request);
     }
