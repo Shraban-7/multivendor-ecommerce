ff9effe67d7b9505ea3761bdbaeb14cb787c0afd
Shraban-7 <shakuatshraban@gmail.com>
2025-12-11 17:09:29 +0600

    flash sale product

--- app/Http/Resources/FlashSaleProductResource.php
+++ app/Http/Resources/FlashSaleProductResource.php
@@ -9,10 +9,13 @@ class FlashSaleProductResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $percentageSold = $this->stock_in > 0 ? ($this->stock_out / $this->stock_in) * 100 : 0;
+        
         return [
             'id' => $this->id,
             'stock_in' => $this->stock_in,
             'stock_out' => $this->stock_out,
+            'percentage_sold' => round($percentageSold, 2),
             'product' => ProductListResource::make($this->product)
         ];
     }
