87d09d75a7057c8c2c82dc596d3a20507a848e66
Shraban-7 <shakuatshraban@gmail.com>
2025-12-17 11:59:03 +0600

    favicon added

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <x-favicons />
+    @stack('meta')
     <title>@yield('title') | eCommerce Marketplace</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
