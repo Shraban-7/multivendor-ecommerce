a950a4d5f2518a00aa761c14846fa0f534f56581
Shraban-7 <shakuatshraban@gmail.com>
2026-01-11 11:21:39 +0600

    add discount and selling price on receipt

---
+++ resources/views/seller/orders/receipt-old.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Order #{{ $order->invoice_id }} Receipt</title>
+    <style>
+        body {
+            font-family: "Courier New", "OCR A Std", monospace;
+            font-size: 13px;
+            width: 302px;
+            margin: 0 auto;
+            font-weight: 600;
+            color: #000;
+            -webkit-print-color-adjust: exact;
+            print-color-adjust: exact;
+        }
+        .left {text-align: start;}
+        .right {text-align: end;}
+        .center {text-align: center;}
+        .line {border-top: 1px dashed #000; margin: 5px 0;}
+        table {width: 100%; border-collapse: collapse;}
+        table td {
+            padding: 2px 0;
+            /* font-weight: 600; */
+        }
+        h3 {margin-bottom: 5px;}
+        p {margin-top: 5px; margin-bottom: 8px;}
+        @media print {
+            body, td, p, h3 {
+                color: #000 !important;
+                font-weight: 600 !important;
+            }
+            .bold {
+                font-weight: 700 !important;
+            }
+        }
+    </style>
+
+</head>
+
+<?php
+    $settings = settings();
+    $showCurrency = false;
+?>
+
+<body>
+    <div class="center">
+        <h3 class="bold">{{ $order->seller->business_name }}</h3>
+        <p>{{ $order->seller->business_address }}</p>
+        <p> Phone: {{ $order->seller->phone }}</p>
+    </div>
+
+    <div class="line" style="margin-top: 20px;"></div>
+
+    <p>
+        <div style="display: flex; justify-content: space-between; margin-bottom:10px;">
+            <span>#{{ $order->invoice_id }}</span>
+            <span>{{ $order->created_at->format('d/m/y h:ia') }}</span>
+        </div>
+
+        @if (!is_null($order->customer_id))
+        Customer: {{ $order->customer->name }}<br>
+        Phone: {{ $order->customer->phone }}
+        @endif
+    </p>
+
+    <div class="line"></div>
+
+    <table>
+        <thead>
+            <tr>
+                <th class="left" style="width: 70%">Item</th>
+                <th class="center" style="width: 10%">Qty</th>
+                <th class="right" style="width: 20%">Total</th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="line"></div>
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($order->items as $item)
+            <tr>
+                <td class="left">{{ $item->product->name }} @if ($item->variant)
+                    <small>({{ $item->variant->fullName }})</small>
+                @endif </td>
+                <td class="center">{{ $item->quantity }}</td>
+                <td class="right">{{ money($item->unit_price * $item->quantity, $showCurrency) }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="line"></div>
+
+    <table>
+        <!-- <tr>
+            <td class="left">Subtotal</td>
+            <td class="right">{{ money($order->sub_total, $showCurrency) }}</td>
+        </tr> -->
+        <!-- @if ($order->discount > 0)
+        <tr>
+            <td class="left">Discount</td>
+            <td class="right">{{ money($order->discount, $showCurrency) }}</td>
+        </tr>
+        @endif -->
+        <!-- <tr>
+            <td colspan="2">
+                <div class="line"></div>
+            </td>
+        </tr> -->
+        <tr class="totals">
+            <td class="left"><strong>TOTAL</strong></td>
+            <td class="right"><strong>{{ money($order->total, $showCurrency) }}</strong></td>
+        </tr>
+    </table>
+
+    @if ($order->due > 0)
+    <div class="line"></div>
+    <table>
+        <tr>
+            <td class="left">Amount Paid</td>
+            <td class="right">{{ money($order->paid, $showCurrency) }}</td>
+        </tr>
+        <tr>
+            <td class="left"><strong>Amount Due</strong></td>
+            <td class="right"><strong>{{ money($order->due, $showCurrency) }}</strong></td>
+        </tr>
+    </table>
+    @endif
+
+    <div class="line"></div>
+
+    <div class="center">
+        <p style="margin-top: 20px;">Thank you for shopping with us!</p>
+        <p>www.slash-mart.com</p>
+    </div>
+</body>
+
+</html>
--- resources/views/seller/orders/receipt.blade.php
+++ resources/views/seller/orders/receipt.blade.php
@@ -15,22 +15,53 @@
             -webkit-print-color-adjust: exact;
             print-color-adjust: exact;
         }
-        .left {text-align: start;}
-        .right {text-align: end;}
-        .center {text-align: center;}
-        .line {border-top: 1px dashed #000; margin: 5px 0;}
-        table {width: 100%; border-collapse: collapse;}
+
+        .left {
+            text-align: start;
+        }
+
+        .right {
+            text-align: end;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .line {
+            border-top: 1px dashed #000;
+            margin: 5px 0;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
         table td {
             padding: 2px 0;
             /* font-weight: 600; */
         }
-        h3 {margin-bottom: 5px;}
-        p {margin-top: 5px; margin-bottom: 8px;}
+
+        h3 {
+            margin-bottom: 5px;
+        }
+
+        p {
+            margin-top: 5px;
+            margin-bottom: 8px;
+        }
+
         @media print {
-            body, td, p, h3 {
+
+            body,
+            td,
+            p,
+            h3 {
                 color: #000 !important;
                 font-weight: 600 !important;
             }
+
             .bold {
                 font-weight: 700 !important;
             }
@@ -40,8 +71,8 @@
 </head>
 
 <?php
-    $settings = settings();
-    $showCurrency = false;
+$settings = settings();
+$showCurrency = false;
 ?>
 
 <body>
@@ -54,15 +85,15 @@
     <div class="line" style="margin-top: 20px;"></div>
 
     <p>
-        <div style="display: flex; justify-content: space-between; margin-bottom:10px;">
-            <span>#{{ $order->invoice_id }}</span>
-            <span>{{ $order->created_at->format('d/m/y h:ia') }}</span>
-        </div>
+    <div style="display: flex; justify-content: space-between; margin-bottom:10px;">
+        <span>#{{ $order->invoice_id }}</span>
+        <span>{{ $order->created_at->format('d/m/y h:ia') }}</span>
+    </div>
 
-        @if (!is_null($order->customer_id))
+    @if (!is_null($order->customer_id))
         Customer: {{ $order->customer->name }}<br>
         Phone: {{ $order->customer->phone }}
-        @endif
+    @endif
     </p>
 
     <div class="line"></div>
@@ -82,13 +113,27 @@
         </thead>
         <tbody>
             @foreach ($order->items as $item)
-            <tr>
-                <td class="left">{{ $item->product->name }} @if ($item->variant)
-                    <small>({{ $item->variant->fullName }})</small>
-                @endif </td>
-                <td class="center">{{ $item->quantity }}</td>
-                <td class="right">{{ money($item->unit_price * $item->quantity, $showCurrency) }}</td>
-            </tr>
+                <tr>
+                    <td class="left">
+                        <div>{{ $item->product->name }}</div>
+
+                        @if ($item->variant)
+                            <small class="text-muted d-block">
+                                {{ $item->variant->fullName }}
+                            </small>
+                        @endif
+                    </td>
+
+                    <td class="center">{{ $item->quantity }}</td>
+                    <td class="right">
+                        @if ($item->selling_price > $item->unit_price)
+                            <span style="text-decoration: line-through;">
+                                {{ money($item->selling_price * $item->quantity, $showCurrency) }}
+                            </span>
+                        @endif
+                        {{ money($item->unit_price * $item->quantity, $showCurrency) }}
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
@@ -96,16 +141,16 @@
     <div class="line"></div>
 
     <table>
-        <!-- <tr>
-            <td class="left">Subtotal</td>
-            <td class="right">{{ money($order->sub_total, $showCurrency) }}</td>
-        </tr> -->
-        <!-- @if ($order->discount > 0)
         <tr>
-            <td class="left">Discount</td>
-            <td class="right">{{ money($order->discount, $showCurrency) }}</td>
+            <td class="left">SUBTOTAL</td>
+            <td class="right">{{ money($order->sub_total, $showCurrency) }}</td>
         </tr>
-        @endif -->
+        @if ($order->discount > 0)
+            <tr>
+                <td class="left">DISCOUNT</td>
+                <td class="right">{{ money($order->discount, $showCurrency) }}</td>
+            </tr>
+        @endif
         <!-- <tr>
             <td colspan="2">
                 <div class="line"></div>
@@ -118,17 +163,17 @@
     </table>
 
     @if ($order->due > 0)
-    <div class="line"></div>
-    <table>
-        <tr>
-            <td class="left">Amount Paid</td>
-            <td class="right">{{ money($order->paid, $showCurrency) }}</td>
-        </tr>
-        <tr>
-            <td class="left"><strong>Amount Due</strong></td>
-            <td class="right"><strong>{{ money($order->due, $showCurrency) }}</strong></td>
-        </tr>
-    </table>
+        <div class="line"></div>
+        <table>
+            <tr>
+                <td class="left">Amount Paid</td>
+                <td class="right">{{ money($order->paid, $showCurrency) }}</td>
+            </tr>
+            <tr>
+                <td class="left"><strong>Amount Due</strong></td>
+                <td class="right"><strong>{{ money($order->due, $showCurrency) }}</strong></td>
+            </tr>
+        </table>
     @endif
 
     <div class="line"></div>
