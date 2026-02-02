2807b753fc9d006ce7ee0431c8726955042f3089
Shraban-7 <shakuatshraban@gmail.com>
2026-02-02 16:31:54 +0600

    invoice

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -29,7 +29,7 @@ class Order extends Model
 
     public function billing_address(): HasOne
     {
-        return $this->hasOne(OrderBillingAddress::class);
+        return $this->hasOne(OrderBillingAddress::class,'order_id');
     }
 
     public function statusLogs()
